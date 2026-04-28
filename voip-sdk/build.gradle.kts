80815fa107f914fb7aa90f47c1cbbc0230210f03
kharozim <kharozim.wrk@gmail.com>
2026-04-28 14:06:26 +0700

    Update version to 1.0.12

--- voip-sdk/build.gradle.kts
+++ voip-sdk/build.gradle.kts
@@ -43,7 +43,7 @@ publishing {
       // Format yang digunakan JitPack: com.github.[User]:[Repo]:[Versi Tag]
       groupId = "com.github.kharozim"
       artifactId = "voip-sdk"
-      version = "1.0.11"
+      version = "1.0.12"
 
       // Ini akan mengambil komponen 'release' yang berisi AAR yang di-generate Android
       afterEvaluate {
